b98bd538461d26774a9462c49e4ad210a777c053
Ivan Manolov <ivanmanolov90@gmail.com>
2015-03-28 13:53:56 -0400

    first commit, setup gulp file with livereload, watching of js and css files, wiredep

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp'),
+	jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	watch = require('gulp-watch'),
+	livereload = require('gulp-livereload'),
+	connect = require('gulp-connect'),
+	source = require('vinyl-source-stream'),
+	wiredep = require('wiredep').stream,
+	browserify = require('browserify');
+
+
+/* 
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+*/
+
+
+gulp.task('browserify', function() {
+    // Single entry point to browserify 
+    return browserify ('./app/js/app.js')
+        .bundle ()
+        //Pass desired output filename to vinyl-source-stream
+        .pipe(source('app-bundle.js'))
+        // Start piping stream to tasks!
+        .pipe(gulp.dest('./app/js')),
+        pipe(connect.reload());
+})
+
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+  gulp.src('./app/styles/*.css')
+    .pipe(connect.reload());
+});
+
+gulp.task('bower', function () {
+  gulp.src('./app/index.html')
+    .pipe(wiredep({
+    	directory: './vendor/',
+    	bowerJson: require('./bower.json')
+    }))
+    .pipe(gulp.dest('./app'));
+});
+
+gulp.task('webserver', function() {
+  connect.server({
+  	root: 'app',
+    livereload:true
+  });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./app/js/*/*.js', ['browserify'])
+    gulp.watch('./app/styles/*.css', ['css'])
+    gulp.watch('./app/*.html', ['html']),
+    gulp.watch('bower.json', ['bower'])
+});
+
+gulp.task('default', ['bower', 'watch', 'webserver', 'browserify']);
+
+
